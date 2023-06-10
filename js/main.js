9dfb449cff275ebd5dc6277a49bccbb28698a266
Joan Francesc Konate <joan.francesc.konate@ticxcat.com>
2023-06-10 23:22:35 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+import * as THREE from '../node_modules/three/build/three.module.js';
+import { TrackballControls } from '../node_modules/three/examples/jsm/controls/TrackballControls.js';
+// Scene
+const scene = new THREE.Scene();
+// Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.6, 1200);
+// Renderer
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor("#233143");
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+// Make Canvas Responsive
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
